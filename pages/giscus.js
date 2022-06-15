b7441a23a95008dbb2f1d09c89cdf319306cdad4
beet <63141491+beetcb@users.noreply.github.com>
2022-06-15 16:49:12 +0800

    giscus needs to be seft hosted due to CSP

--- pages/giscus.js
+++ pages/giscus.js
@@ -1,25 +1,24 @@
+import Giscus from '@giscus/react';
 import { useRouter } from 'next/router';
 
 function Discus() {
-  const router = useRouter();
-  const { query } = router;
+  const pageId = useRouter().query.id;
 
   return (
-    <script
-      src="https://giscus.app/client.js"
-      data-repo="[在此输入仓库]"
-      data-repo-id="[在此输入仓库 ID]"
-      data-category="[在此输入分类名]"
-      data-category-id="[在此输入分类 ID]"
-      data-mapping="pathname"
-      data-reactions-enabled="1"
-      data-emit-metadata="0"
-      data-input-position="bottom"
-      data-theme="light"
-      data-lang="zh-CN"
-      crossOrigin="anonymous"
-      async
-    ></script>
+    <Giscus
+      repo="beetcb/comments"
+      repoId="R_kgDOHgWYoQ"
+      category="Announcements"
+      categoryId="DIC_kwDOHgWYoc4CPrmU"
+      mapping="specific"
+      term={pageId}
+      reactionsEnabled="1"
+      emitMetadata="0"
+      inputPosition="top"
+      theme="light"
+      lang="zh-CN"
+      loading="lazy"
+    ></Giscus>
   );
 }
 
--- pages/index.js
+++ pages/index.js
@@ -1,16 +1,3 @@
-import { useEffect } from 'react';
-import { useState } from 'react';
-
 export default function Home() {
-  let [pURL, setPURL] = useState('NULL');
-  useEffect(() => {
-    const pURL =
-      window.location != window.parent.location
-        ? document.referrer
-        : document.location.href;
-    if (pURL) {
-      setPURL(pURL);
-    }
-  }, []);
-  return <h1>The parent url is [{pURL}]</h1>;
+  return <h1>Hello</h1>;
 }
